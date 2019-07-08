23decf3f1184c7ffe9e86d896224364d22988110
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-07-08 14:48:37 -0400

    Added and hooked up Meta.js.

--- components/Page.js
+++ components/Page.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Meta from './Meta';
 
 class Page extends Component {
   render() {
     return (
+      <>
+      <Meta />
       <p>Hi, page is hooked up.</p>
+      </>
     )
   }
 }
